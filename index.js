d58d0fe731ad395ce1a87e12e87e3db65de58a6f
prince-sunsara <princesunasara977@gmail.com>
2022-09-23 16:45:29 +0530

    file uploaded

---
+++ index.js
@@ -0,0 +1,82 @@
+const fs = require("fs");
+const http = require("http");
+
+// create objects to store the information
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+let styleContent = "";
+let scriptContent = "";
+
+// read the files from the local folders
+fs.readFile("./home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+fs.readFile("./project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+fs.readFile("./registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
+fs.readFile("./script.js", (err, script) => {
+  if (err) {
+    throw err;
+  }
+  scriptContent = script;
+});
+fs.readFile("./style.css", (err, style) => {
+  if (err) {
+    throw err;
+  }
+  styleContent = style;
+});
+
+// use the HTTP server to rander the above content in a port of our choice...
+fs.readFile("home.html", (err, home) => {
+    if (err) {
+        throw err;
+      }
+      http
+        .createServer((req, res) => {
+          let url = req.url;
+          res.writeHead(200, { "Content-Type": "text/html" });
+          switch (url) {
+            case "/project":
+              res.write(projectContent);
+              res.end();
+              break;
+    
+            case "/registration":
+              res.write(registrationContent);
+              res.end();
+              break;
+    
+            case "/style.css":
+              res.writeHead(200, { "Content-Type": "text/css" });
+              res.write(styleContent);
+              res.end();
+              break;
+    
+            case "/script.js":
+              res.writeHead(200, { "Content-Type": "text/javascript" });
+              res.write(scriptContent);
+              res.end();
+              break;
+    
+            default:
+              res.write(homeContent);
+              res.end();
+              break;
+          }
+        })
+        .listen(5000);
+});
---
+++ script.js
@@ -0,0 +1,92 @@
+let userform = document.getElementById("form");
+
+const retrieveEntries = () => {
+    let entries = localStorage.getItem('user-entries');
+    if (entries) {
+        entries = JSON.parse(entries);
+    } else {
+        entries = [];
+    }
+    return entries;
+}
+
+let userEntries = retrieveEntries();
+
+const displayEntries = () => {
+    const entries = retrieveEntries();
+    console.log(entries);
+
+    const tableEntries = entries.map((entry) => {
+        const nameCell = `<td align="center" style="padding:4px 8px">${entry.name}</td>`;
+        const emailCell = `<td align="center" style="padding:4px 8px">${entry.email}</td>`;
+        const passwordCell = `<td align="center" style="padding:4px 8px">${entry.password}</td>`;
+        const dobCell = `<td align="center" style="padding:4px 8px">${entry.dob}</td>`;
+        const termsCell = `<td align="center" style="padding:4px 8px">${entry.terms}</td>`;
+
+        const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${termsCell}</tr>`;
+        return row;
+    }).join("\n");
+
+    const table = `<table border="2" style="margin-top:10px;"><tr>
+    <th align="center" style="padding:12px;">Name</th>
+    <th align="center" style="padding:12px;">Email</th>
+    <th align="center" style="padding:12px;">Password</th>
+    <th align="center" style="padding:12px;">Dob</th>
+    <th align="center" style="padding:12px;">Accepted terms?</th>
+    </tr>${tableEntries}</table>`;
+
+    let details = document.getElementById("user-entries");
+    details.innerHTML = table;
+}
+
+const saveUserForm = (event) =>{
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const dob = document.getElementById('dob').value;
+    const terms = document.getElementById('terms').checked;
+    
+
+    const entry = {
+        name,
+        email,
+        password,
+        dob,
+        terms   
+    };
+    userEntries.push(entry);
+    
+    localStorage.setItem("user-entries", JSON.stringify(userEntries));
+    displayEntries();
+}
+userform.addEventListener('submit', saveUserForm);
+displayEntries();
+
+    let dateElement = document.getElementById("dob");
+    
+dateElement.addEventListener("change",() => {
+    let [year,month,date] = document.getElementById("dob").value.split("-");
+    let dob = new Date(year,month,date);
+    let age = getAge(dob);
+
+    if (age< 18 || age > 55) {
+        dateElement.setCustomValidity("Your age doesn't lies between 18 and 55.")
+    } else {
+        dateElement.setCustomValidity("");
+    }
+});
+
+function getAge(date) {
+    let today = new Date();
+    let birthDate = new Date(date);
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = today.getMonth() - birthDate.getMonth();
+
+    if (m<0 || (m === 0 && birthDate.getDate() < birthDate.getDate()) ) {
+        age --;
+    }
+    return age;
+}
+
