0d6d75c81bd54e21569b8a49abf420e08fdf2f6c
prince-sunsara <princesunasara977@gmail.com>
2022-09-23 17:07:33 +0530

    index.js file updated

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
 const http = require("http");
+const readline = require("minimist");
+
+const args = require("minimist")(process.argv.slice(2));
+let port = args.port;
 
 // create objects to store the information
 let homeContent = "";
@@ -41,42 +45,42 @@ fs.readFile("./style.css", (err, style) => {
 });
 
 // use the HTTP server to rander the above content in a port of our choice...
-fs.readFile("home.html", (err, home) => {
-    if (err) {
-        throw err;
-      }
-      http
-        .createServer((req, res) => {
-          let url = req.url;
-          res.writeHead(200, { "Content-Type": "text/html" });
-          switch (url) {
-            case "/project":
-              res.write(projectContent);
-              res.end();
-              break;
-    
-            case "/registration":
-              res.write(registrationContent);
-              res.end();
-              break;
-    
-            case "/style.css":
-              res.writeHead(200, { "Content-Type": "text/css" });
-              res.write(styleContent);
-              res.end();
-              break;
-    
-            case "/script.js":
-              res.writeHead(200, { "Content-Type": "text/javascript" });
-              res.write(scriptContent);
-              res.end();
-              break;
-    
-            default:
-              res.write(homeContent);
-              res.end();
-              break;
-          }
-        })
-        .listen(5000);
+// fs.readFile("home.html", (err, home) => {
+//     if (err) {
+//         throw err;
+//       }
+const server = http.createServer((req, res) => {
+  let url = req.url;
+  res.writeHead(200, { "Content-Type": "text/html" });
+  switch (url) {
+    case "/project":
+      res.write(projectContent);
+      res.end();
+      break;
+
+    case "/registration":
+      res.write(registrationContent);
+      res.end();
+      break;
+
+    case "/style.css":
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.write(styleContent);
+      res.end();
+      break;
+
+    case "/script.js":
+      res.writeHead(200, { "Content-Type": "text/javascript" });
+      res.write(scriptContent);
+      res.end();
+      break;
+
+    default:
+      res.write(homeContent);
+      res.end();
+      break;
+  }
+});
+server.listen(port, "127.0.0.1", () => {
+  console.log(`listening at port ${port}`);
 });
